403914b3cac9ac30be6d3cb8910113e4126d358c
Roberto Di Remigio <roberto.diremigio@gmail.com>
2020-11-20 13:45:03 +0100

    Go back to sphinx-rtd-theme
    Add CSS overrides for colors

--- content/conf.py
+++ content/conf.py
@@ -69,24 +69,15 @@ exclude_patterns = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_book_theme"
+html_theme = "sphinx_rtd_theme"
 html_logo = "img/ENCCS.jpg"
+html_favicon = "img/favicon.ico"
 html_title = project
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-
-# configuration of sphinx_book_theme
-html_theme_options = {
-    "repository_url": f"https://github.com/{github_user}/{github_repo_name}",
-    "use_repository_button": True,
-    "use_edit_page_button": True,
-    "path_to_docs": "content",
-    "use_download_button": False,
-}
-
+html_static_path = ['_static']
 
 # HTML context:
 from os.path import dirname, realpath, basename
@@ -116,3 +107,6 @@ todo_include_todos = True
 rst_epilog = f"""
 {MPI_glossary()}
 """
+
+def setup(app):
+  app.add_css_file("overrides.css")
