6540864002af33aa7dbd51a314be7b87b25e9b5b
Mark Abraham <mark.j.abraham@gmail.com>
2020-12-07 18:23:38 +0100

    Update with exercise

--- content/code/threading-query-solution.c
+++ content/code/threading-query-solution.c
@@ -3,19 +3,19 @@
 
 int main(int argc, char **argv)
 {
-    /* ==== CHALLENGE ====
-     *
-     * Uncomment and fix the MPI call to make this code work! Start
-     * an MPI environment suitable for OpenMP fork-join parallelism.
-     */
-
     /* Initialize the MPI environment and report */
     int required, provided;
     required = MPI_THREAD_FUNNELED;
     MPI_Init_thread(NULL, NULL, required, &provided);
-    /* MPI_xxx(xxx); */
         
     fprintf(stdout, "Initialization required %d and %d was provided\n", required, provided);
+
+    MPI_Query_thread(&provided);
+    fprintf(stdout, "The thread query reported that level %d was provided\n", provided);
+
+    int is_master;
+    MPI_Is_thread_main(&is_master);
+    fprintf(stdout, "Is this the main thread on rank %d? %s\n", is_master ? "yes" : "no");
 
     /* Clean up and exit */
     MPI_Finalize();
